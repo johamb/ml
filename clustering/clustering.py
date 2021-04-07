e12c243e363aeb180c3aff8d68af80946a5f0c2c
joham <mail@joham.dev>
2021-04-07 09:05:45 +0200

    clustering

---
+++ clustering/clustering.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+# example function definition
+def testFunction(data):
+    print(data)
+    datasum = np.sum(data)
+    return datasum
+
+
+
+# main program code
+
+# read data from file
+data_pd = pd.read_csv('data.txt', header=None, sep='\s+')
+
+# convert pandas to numpy array
+# access elements as data[row,col]
+data = data_pd.values
+
+# example for function call
+sumdata = testFunction(data)
+print("Sum: ", sumdata)
+
+# plot data as scatter plot
+plt.plot(data[:,0],data[:,1],'b+')
+plt.show()
